b43b7b07edab34594a388023012099a9acbcb28a
dishana thevanantham <dishana4@gmail.com>
2023-12-22 09:35:27 +0100

    aggiunta file js

---
+++ script.js
@@ -0,0 +1,6 @@
+let sezioneNascondi = document.getElementById("contenitore")
+let btnSubscribe = document.getElementById("subscrine")
+
+function mostraNascondi() {
+    sezioneNascondi.style.display = "none"; 
+}
